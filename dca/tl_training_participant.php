c18277a88ff0b068d8f9b7e7211c4ad6d12fad2b
Andreas Schempp <andreas@schempp.ch>
2012-06-29 09:39:21 +0200

    Removed global operation to edit all participants

--- dca/tl_training_participant.php
+++ dca/tl_training_participant.php
@@ -57,16 +57,6 @@ $GLOBALS['TL_DCA']['tl_training_participant'] = array
 			'fields'					=> array('gender', 'firstname', 'lastname'),
 			'format'					=> '%s %s %s',
 		),
-		'global_operations' => array
-		(
-			'all' => array
-			(
-				'label'					=> &$GLOBALS['TL_LANG']['MSC']['all'],
-				'href'					=> 'act=select',
-				'class'					=> 'header_edit_all',
-				'attributes'			=> 'onclick="Backend.getScrollOffset();" accesskey="e"'
-			),
-		),
 		'operations' => array
 		(
 			'edit' => array
